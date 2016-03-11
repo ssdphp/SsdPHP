cbfc6f9b3aaea556bece96585b73a970ee1a3a60
谢智辉 <xzh_tx@163.com>
2016-03-11 14:50:11 +0800

    创建

---
+++ config/email.php
@@ -0,0 +1,33 @@
+<?php
+/*{{{LICENSE
++-----------------------------------------------------------------------+
+| SsdPHP Framework                                                   |
++-----------------------------------------------------------------------+
+| This program is free software; you can redistribute it and/or modify  |
+| it under the terms of the GNU General Public License as published by  |
+| the Free Software Foundation. You should have received a copy of the  |
+| GNU General Public License along with this program.  If not, see      |
+| http://www.gnu.org/licenses/.                                         |
+| Copyright (C) 2015-2020. All Rights Reserved.                         |
++-----------------------------------------------------------------------+
+| Supports: http://www.SsdPHP.com                                    |
++-----------------------------------------------------------------------+
+}}}*/
+//redis配置
+return array(
+
+    'Email'=>array(
+        'Main'=>array(
+            //SMTP servers
+            'Host'=>"smtp.mxhichina.com",
+            //SMTP username  注意：普通邮件认证不需要加 @域名
+            'Username'=>"postmaster@xxxx.com",
+            //SMTP password
+            'Password'=>"xxxx",
+            //发件人邮箱
+            'From'=>"postmaster@bynxl.com",
+            //发件人昵称
+            'FromName'=>"www.ssdphp.com",
+        ),
+    )
+);
