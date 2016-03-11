7f95f8131b40092881325498f167a266b55c5001
谢智辉 <xzh_tx@163.com>
2016-03-11 16:52:03 +0800

    创建

--- www/index.php
+++ www/index.php
@@ -1,12 +1,17 @@
 <?php
+define("start",microtime(true));
 if(!class_exists('\SsdPHP\SsdPHP',false))
 {
     require(dirname(dirname(__FILE__)).DIRECTORY_SEPARATOR."SsdPHP".DIRECTORY_SEPARATOR."SsdPHP.php");
 }
-echo SsdPHP\SsdPHP::bootstrap(function ($rootPath=""){
+echo SsdPHP\SsdPHP::bootstrap(function (){
     SsdPHP\Pulgins\Common\RegShutdownEvent::register();
+    #SsdPHP\SsdPHP::setDebug(false);
     SsdPHP\Pulgins\Common\Error::$LOG     =SsdPHP\SsdPHP::isDebug();
     SsdPHP\Pulgins\Common\Error::$CONSOLE =SsdPHP\SsdPHP::isDebug();
     SsdPHP\Core\Config::load(SsdPHP\SsdPHP::getRootPath().DIRECTORY_SEPARATOR.'config');
     SsdPHP\Pulgins\Common\Route::set(SsdPHP\Core\Config::getField('ROUTE','home'));
 })->run() === false ? "404 error!" : '' ;
+
+define("end",microtime(true));
+echo "<br>".(end-start);
