98fb5b6e574a0bb95ed0ae9c893563f795d5ff52
谢智辉 <xzh_tx@163.com>
2016-03-11 16:22:02 +0800

    创建

--- SsdPHP/Pulgins/Common/RegShutdownEvent.php
+++ SsdPHP/Pulgins/Common/RegShutdownEvent.php
@@ -8,7 +8,9 @@
 // +----------------------------------------------------------------------
 // | Author: xiaohuihui <598550105@qq.com> //
 // +----------------------------------------------------------------------
-namespace pulgins;
+namespace SsdPHP\Pulgins\Common;
+use SsdPHP\SsdPHP;
+
 /**
  * Manage php shutdown events.
  * @author Lancer He <lancer.he@gmail.com>
@@ -22,9 +24,7 @@ class RegShutdownEvent {
      */
     private static $_events = array(
         //致命错误的处理
-        array('SError::fatalError','1'),
-        //日志处理
-        array('SLog::UserActionLog','1')
+        array('SsdPHP\Pulgins\Common\Error::fatalError')
     );
 
 
@@ -32,7 +32,7 @@ class RegShutdownEvent {
      * register shutdown
      */
     public static function register() {
-        register_shutdown_function(array('SRegShutdownEvent', 'call'));
+        register_shutdown_function('SsdPHP\Pulgins\Common\RegShutdownEvent::call');
     }
 
 
@@ -61,6 +61,8 @@ class RegShutdownEvent {
      * call event when you need.
      */
     public static function call() {
+        /* 冲刷(flush)所有响应的数据给客户端并结束请求。 这使得客户端结束连接后，需要大量时间运行的任务能够继续运行 */
+        !SsdPHP::isDebug() && php_sapi_name()=='fpm-fcgi' && fastcgi_finish_request();
         foreach (self::$_events as $event) {
             $callback = array_shift($event);
             call_user_func_array($callback, $event);
