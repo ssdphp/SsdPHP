5fca94719ba9661b2d5f37eac92807be6b4aadd1
谢智辉 <xzh_tx@163.com>
2016-03-11 16:44:07 +0800

    创建

--- Home/controller/index.php
+++ Home/controller/index.php
@@ -17,15 +17,25 @@
 namespace Home\controller;
 
 use SsdPHP\Pulgins\DataBase\Factory as MysqlFactory,
-SsdPHP\Pulgins\Cache\Factory as CacheFactory;
+    SsdPHP\Pulgins\Cache\Factory as CacheFactory,
+    SsdPHP\Pulgins\Common\RegShutdownEvent,
+    SsdPHP\Pulgins\Email\Factory as EmailFactory;
 
 class index{
 
 
     public function index(){
-        $res = MysqlFactory::getInstance()->select("ip");
+        /*$res = MysqlFactory::getInstance()->select("ip");
         CacheFactory::getInstance()->set("ip",50);
-        print_r(CacheFactory::getInstance()->get('ip'));
+        RegShutdownEvent::add('Home\controller\index::taskEmail','xzh_tx@163.com','xhh','test','body');*/
+        echo (123);
+    }
+
+    public static function taskEmail($toEmail,$toUser,$title,$body){
+        if(!$toEmail || !$title){
+            return ;
+        }
+        EmailFactory::getInstance()->send_mail($toEmail,$toUser,$title,$body);
     }
 
 
