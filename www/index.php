0b7333fe8d97e60e6c015b11d76c023d84d5addd
谢智辉 <xzh_tx@163.com>
2016-03-12 09:54:06 +0800

    创建

--- www/index.php
+++ www/index.php
@@ -1,19 +1,14 @@
 <?php
 /* php version >= 5.3.5 */
-define("start",microtime(true));
 if(!class_exists('\SsdPHP\SsdPHP',false))
-{
     require(dirname(dirname(__FILE__)).DIRECTORY_SEPARATOR."SsdPHP".DIRECTORY_SEPARATOR."SsdPHP.php");
-}
-echo SsdPHP\SsdPHP::bootstrap(function (){
+echo
+SsdPHP\SsdPHP::bootstrap(function (){
     date_default_timezone_set('PRC');
     SsdPHP\Pulgins\Common\RegShutdownEvent::register();
-    #SsdPHP\SsdPHP::setDebug(false);
-    SsdPHP\Pulgins\Common\Error::$LOG     =SsdPHP\SsdPHP::isDebug();
-    SsdPHP\Pulgins\Common\Error::$CONSOLE =SsdPHP\SsdPHP::isDebug();
+    SsdPHP\SsdPHP::setDebug(false);
     SsdPHP\Core\Config::load(SsdPHP\SsdPHP::getRootPath().DIRECTORY_SEPARATOR.'config');
     SsdPHP\Pulgins\Common\Route::set(SsdPHP\Core\Config::getField('ROUTE','home'));
-})->run() === false ? "404 error!" : '' ;
 
-define("end",microtime(true));
-echo "<br>".(end-start);
+})->run() === false ? "404 error!" : ''
+;
