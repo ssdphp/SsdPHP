5c0f860146eaaef98b2e25d3db7ffa5bffe79768
谢智辉 <xzh_tx@163.com>
2016-03-12 16:21:09 +0800

    创建 session 管理

---
+++ config/session.php
@@ -0,0 +1,34 @@
+<?php
+/*{{{LICENSE
++-----------------------------------------------------------------------+
+| SsdPHP Framework                                                   |
++-----------------------------------------------------------------------+
+| This program is free software; you can redistribute it and/or modify  |
+| it under the terms of the GNU General Public License as published by  |
+| the Free Software Foundation. You should have received a copy of the  |
+| GNU General Public License along with this program.  If not, see      |
+| http://www.gnu.org/licenses/.                                         |
+| Copyright (C) 2015-2020. All Rights Reserved.                         |
++-----------------------------------------------------------------------+
+| Supports: http://www.SsdPHP.com                                    |
++-----------------------------------------------------------------------+
+}}}*/
+
+/*
+ * Session = array(
+ *     'new_cache_expire' => 30, //存活时间
+ *     'path' => '/',
+ *     'domain' => '',
+ *     'secure' => false,
+ *     'httponly' => true,
+ *     'save_path' => null,
+ *     'session_name' => 'SSDPHPSESSID',
+ * )
+ */
+return array(
+
+    "Session"=>array(
+        'new_cache_expire' => 30, //存活时间
+    ),
+
+);
