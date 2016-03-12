9c92fdf0271782bdc8f80a6648a50420e5fe9992
谢智辉 <xzh_tx@163.com>
2016-03-12 10:22:36 +0800

    创建

--- Home/controller/index.php
+++ Home/controller/index.php
@@ -1,19 +1,4 @@
 <?php
-/*{{{LICENSE
-+-----------------------------------------------------------------------+
-| SsdPHP Framework                                                   |
-+-----------------------------------------------------------------------+
-| This program is free software; you can redistribute it and/or modify  |
-| it under the terms of the GNU General Public License as published by  |
-| the Free Software Foundation. You should have received a copy of the  |
-| GNU General Public License along with this program.  If not, see      |
-| http://www.gnu.org/licenses/.                                         |
-| Copyright (C) 2015-2020. All Rights Reserved.                         |
-+-----------------------------------------------------------------------+
-| Supports: http://www.SsdPHP.com                                    |
-+-----------------------------------------------------------------------+
-}}}*/
-
 namespace Home\controller;
 
 use SsdPHP\Pulgins\DataBase\Factory as MysqlFactory,
@@ -25,9 +10,9 @@ class index{
 
 
     public function index(){
-        /*$res = MysqlFactory::getInstance()->select("ip");
+        $res = MysqlFactory::getInstance()->select("ip");
         CacheFactory::getInstance()->set("ip",50);
-        RegShutdownEvent::add('Home\controller\index::taskEmail','xzh_tx@163.com','xhh','test','body');*/
+        #RegShutdownEvent::add('Home\controller\index::taskEmail','xzh_tx@163.com','xhh','test','body');
         echo (123);
     }
 
