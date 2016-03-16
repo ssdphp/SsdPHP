22feabbdf1390f18abb2811df4782237a3a62998
谢智辉 <xzh_tx@163.com>
2016-03-16 18:02:40 +0800

    创建

--- App/Home/controller/index.php
+++ App/Home/controller/index.php
@@ -4,19 +4,27 @@ namespace App\Home\controller;
 use SsdPHP\Pulgins\DataBase\Factory as MysqlFactory,
     SsdPHP\Pulgins\Cache\Factory as CacheFactory,
     SsdPHP\Pulgins\Common\RegShutdownEvent,
+    SsdPHP\Pulgins\Http\Input,
     SsdPHP\Pulgins\Session\Factory as Session,
     SsdPHP\Pulgins\Email\Factory as EmailFactory;
 
 class index{
 
+    public function __construct()
+    {
+        Session::Start();
+    }
 
     public function index(){
         #$res = MysqlFactory::getInstance()->select("ip");
         #CacheFactory::getInstance()->set("ip",50);
         #RegShutdownEvent::add('Home\controller\index::taskEmail','xzh_tx@163.com','xhh','test','body');
-
+        /*echo Input::get("ni",0,"intval");
         Session::set('nihao',123);
-        echo Session::get('nihao');
+        echo Session::get('nihao');*/
+        // 创建日志频道
+        $log = new Logger('name');
+        $s= new \Smarty();
     }
 
     public static function taskEmail($toEmail,$toUser,$title,$body){
