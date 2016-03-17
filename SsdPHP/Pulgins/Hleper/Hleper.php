23fb748ebd5d9c4cab87880e1bc4e4466edadb48
谢智辉 <xzh_tx@163.com>
2016-03-17 11:56:35 +0800

    创建

--- SsdPHP/Pulgins/Hleper/Hleper.php
+++ SsdPHP/Pulgins/Hleper/Hleper.php
@@ -1,17 +1,6 @@
 <?php
-// +----------------------------------------------------------------------
-// | api.uarein.com
-// +----------------------------------------------------------------------
-// | Copyright (c) 2015 http://www.uarein.com All rights reserved.
-// +----------------------------------------------------------------------
-// | Licensed ( http://www.apache.org/licenses/LICENSE-2.0 )
-// +----------------------------------------------------------------------
-// | Author: xiaohuihui <598550105@qq.com> //
-// +----------------------------------------------------------------------
-namespace SsdPHP\Pulgins\Hleper;
 
-use Mockery\Exception;
-use SsdPHP\SsdPHP;
+namespace SsdPHP\Pulgins\Hleper;
 
 class Hleper{
 
