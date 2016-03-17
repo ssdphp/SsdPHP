6474379b8a285fe3df0a7ad52bb5ab1a085d378d
谢智辉 <xzh_tx@163.com>
2016-03-17 11:06:56 +0800

    创建

---
+++ SsdPHP/Pulgins/View/Factory.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace SsdPHP\Pulgins\View;
+use SsdPHP\Core\Factory as SFactory,
+    SsdPHP\SsdPHP,
+    SsdPHP\Core\Config as SConfig;
+
+class Factory
+{
+
+    public static function getInstance($adapter = 'Smarty', $config = null)
+    {
+        $className = __NAMESPACE__ . "\\Adaptor\\{$adapter}";
+        if(empty($config)){
+            $appdir = SsdPHP::getRootPath().SsdPHP::getAppDir();
+            $model = SsdPHP::getModel();
+            $templates_c = $appdir.DIRECTORY_SEPARATOR.$model.DIRECTORY_SEPARATOR."templates_c";
+            $template_dir= $appdir.DIRECTORY_SEPARATOR.$model.DIRECTORY_SEPARATOR."templates";
+            $templates_config= $appdir.DIRECTORY_SEPARATOR.$model.DIRECTORY_SEPARATOR."templates_config";
+            $templates_plugins= $appdir.DIRECTORY_SEPARATOR.$model.DIRECTORY_SEPARATOR."templates_plugins";
+            $config = SConfig::get($adapter,array(
+                'templates_c'=>$templates_c,
+                'template_dir'=>$template_dir,
+                'templates_config'=>$templates_config,
+                'templates_plugins'=>$templates_plugins,
+                'force_compile'=>true,
+                'debugging'=>true,
+                'caching'=>true,
+                'cache_lifetime'=>120,
+            ));
+        }
+        return SFactory::getInstance($className, $config);
+    }
+
+
+}
