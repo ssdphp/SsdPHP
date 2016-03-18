d9e1700a0747e3193a46ecf5c9ee4cf8e7d9a077
谢智辉 <xzh_tx@163.com>
2016-03-18 10:55:46 +0800

    创建

--- App/Task/Email.php
+++ App/Task/Email.php
@@ -2,6 +2,7 @@
 namespace App\Task;
 
 use SsdPHP\Pulgins\Email\Factory as EmailFactory;
+use SsdPHP\Core\Config as SConfig;
 
 class Email{
 
@@ -24,11 +25,15 @@ class Email{
             'status' => (int)$result,
             'time' => time(),
             'content' => $content,
-            'type' => SConfig::getField('Email','EmailType'),
+            'type' => SConfig::getField('Email','Main')['EmailType'],
         );
         if(is_callable($callback)){
             call_user_func_array($callback,array("data"=>$callbackData));
         }
+    }
+
+    public static function callback($data){
+        // todo
     }
 
 }
