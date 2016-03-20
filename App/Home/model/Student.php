0f448a4f9cb4618aabe2fc4ffec84914572d05f0
谢智辉 <xzh_tx@163.com>
2016-03-20 11:29:49 +0800

    增加

---
+++ App/Home/model/Student.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace  App\Home\model;
+
+
+class Student{
+
+    public function Login($account,$pwd){
+
+        if(empty($account) || empty($pwd)){
+            return -1;
+        }
+        
+
+    }
+}
