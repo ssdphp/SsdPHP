4dbe4341bf37f8b012cf7d3450cab652982d1336
谢智辉 <xzh_tx@163.com>
2016-03-20 13:10:23 +0800

    创建

---
+++ vendor/ssdphp/SsdPHP/Pulgins/Common/Route.php
@@ -0,0 +1,42 @@
+<?php
+namespace SsdPHP\Pulgins\Common;
+use SsdPHP\SsdPHP;
+
+/**
+ * Class Route
+ * @author xiaohuihui <xzh_tx@163.com>
+ * @package SsdPHP\Pulgins
+ */
+class Route{
+    private static $_Routes=array();
+
+    public static function set(array $route){
+        self::$_Routes = $route;
+        if(!empty(self::$_Routes)){
+
+            self::parse();
+        }
+    }
+
+    /**
+     * 支持cli
+     * author xiaohuihui <xzh_tx@163.com>
+     */
+    private static function parse(){
+        if(PHP_SAPI=='cli'){
+            $_SERVER['PATH_INFO']=isset($_SERVER['argv'][1]) ? $_SERVER['argv'][1] : '/';
+        }
+        $REQUEST_URI = !empty($_SERVER['PATH_INFO'])?$_SERVER['PATH_INFO']:$_SERVER['REQUEST_URI'];
+
+        foreach(self::$_Routes as $pattern=>$pathInfo){
+
+            preg_match("$pattern",$REQUEST_URI,$r);
+            if(isset($r[0]) && !empty($r)){
+                $url = preg_replace($pattern,$pathInfo,$REQUEST_URI);
+                SsdPHP::setPathInfo($url);
+                break;
+            }
+        }
+        return ;
+    }
+}
